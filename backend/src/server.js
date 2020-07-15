25da441c2a3b19cbd8e0dd69caee14112360e072
leo-goss <leo.goss@icloud.com>
2020-07-15 16:34:44 -0300

    devradar

---
+++ backend/src/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
+require('dotenv').config();
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect(process.env.MONGO_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: true
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(process.env.PORT || 3333);
